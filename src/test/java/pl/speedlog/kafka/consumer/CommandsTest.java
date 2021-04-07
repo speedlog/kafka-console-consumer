e29ea4091f791785e8e8a5a14f9551eb7ddfe83d
Mariusz Wyszomierski <mariusz.wyszomierski@coi.gov.pl>
2021-04-07 22:50:26 +0200

    Return application version
    Fixes #6

--- src/test/java/pl/speedlog/kafka/consumer/CommandsTest.java
+++ src/test/java/pl/speedlog/kafka/consumer/CommandsTest.java
@@ -4,6 +4,7 @@ import com.github.blindpirate.extensions.CaptureSystemOutput;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.matchesPattern;
 import static org.mockito.Mockito.mock;
 
 class CommandsTest {
@@ -22,6 +23,13 @@ class CommandsTest {
 	void invalidTimestamp(CaptureSystemOutput.OutputCapture outputCapture) {
 		outputCapture.expect(containsString("Invalid value for option '--timestamp': 'invalid' is not a long"));
 		commands.run("--topic=example-topic", "--timestamp=invalid");
+	}
+
+	@Test
+	@CaptureSystemOutput
+	void shouldPrintApplicationVersion(CaptureSystemOutput.OutputCapture outputCapture) {
+		outputCapture.expect(matchesPattern("Version: \\d+\\.\\d+\\.\\d+.*\\n.*\\n"));
+		commands.run("--version");
 	}
 
 }
